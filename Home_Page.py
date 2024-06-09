080ff46ba1887e1f22073975e747845c098d5318
Saumitra Panchal <saumitrapanchal@gmail.com>
2024-06-09 11:44:51 +0530

    Update Home_Page.py
    Added data ref

--- Home_Page.py
+++ Home_Page.py
@@ -17,3 +17,12 @@ One particularly notable industry is **Forestry and support**. Since 2018, it ha
             
 **To observe additional trends, please interact with the visuals in this dashboard.**
 ''')
+
+st.empty()
+st.divider()
+
+label = '''
+Data Ref: [Statistics Canada. Table 14-10-0201-01  Employment by industry, monthly, unadjusted for seasonality](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1410020101)
+'''
+
+st.write(label)
